b318c6c1cca80e32dd5793744f8bbdbbfdb95e3e
jjunysgit <061284yong@gmail.com>
2023-06-09 03:43:02 +0900

    Add files via upload

---
+++ controleng-1/test.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+def main():
+    st.title("LEE YONG JUN")
+    st.text('hello my homepage')
+    st.header('header')
+    st.subheader('subheader')
+
+    st.markdown('# zzzz')
+    st.markdown('# zzzz')
+    st.markdown('# zzzz')
+
+    st.success("success")
+    st.warning("warning")
+    st.info('info')
+    st.error('errror')
+    st.exception('exception')
+       
+
+
+
+if __name__ =='__main__' :
+    main()
+
+
