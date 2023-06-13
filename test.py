7a82149b44094c630ee8755209580f6f88dad2a2
jjunysgit <061284yong@gmail.com>
2023-06-13 18:28:02 +0900

    gsdgsadf

--- test.py
+++ test.py
@@ -17,8 +17,8 @@ G1 = control.feedback(G)
 
 G1_str1 = f"{G1.num[0][0][0]}"
 G1_str2 = f"{G1.den[0][0][0]} s^2 + {G1.den[0][0][1]} s + {G1.den[0][0][2]}"
-st.write("피드백 전달함수",G1_str1)
-st.write(G1_str2)
+st.write(G1_str1,"\n",G1_str2)
+
 
 #단위 계단 응답
 num = [100]
