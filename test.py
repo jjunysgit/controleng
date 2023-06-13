767d98f2f75a8d89fdf9cf1ae8558bbc9745716e
jjunysgit <061284yong@gmail.com>
2023-06-13 18:35:18 +0900

    asfgdgs

--- test.py
+++ test.py
@@ -17,9 +17,9 @@ G1 = control.feedback(G)
 
 G1_str1 = f"{G1.num[0][0][0]}"
 G1_str2 = f"{G1.den[0][0][0]} s^2 + {G1.den[0][0][1]} s + {G1.den[0][0][2]}"
-st.write("\t",G1_str1)
-st.write("ㅡㅡㅡㅡㅡㅡㅡ")
-
+st.write(G1_str1)
+st.write("---------")
+st.write(G1_str2)
 
 #단위 계단 응답
 num = [100]
